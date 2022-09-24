3c51e9fc78162654fceb038dd319914ed23bf640
alex Bedolla <j.alexbedolla.a@gmail.com>
2022-09-24 00:10:18 -0600

    agregando los estados

--- React la Guia completa/administrador de pacientes/src/components/Formulario.jsx
+++ React la Guia completa/administrador de pacientes/src/components/Formulario.jsx
@@ -3,10 +3,13 @@ import { useState, useEffect } from 'react'
 
 const Formulario = () => {
   const [mascota, setMascota] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintomas, setSintomas] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefaul()
-    console.log('Enviando')
   }
 
   return (
@@ -20,49 +23,77 @@ const Formulario = () => {
         className=' bg-white shadow-md rounded-lg py-10 px-5 mb-10'
       >
         <div className='mb-5'>
-          <label htmlFor='pet' className=' block text-violet-600 uppercase font-bold'>
+          <label htmlFor='mascota' className=' block text-violet-600 uppercase font-bold'>
             Nombre de la Mascota
           </label>
           <input
-            id='pet'
+            id='mascota'
             type='text'
-            pceholder='pet name'
-            className=' border-2 w-full p-2 mt-2 placehogray-600 rounded-md'
+            placeholder='Nombre de la Mascota'
+            className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md'
             value={mascota}
             onChange={(e) => setMascota(e.target.value)}
           />
-
         </div>
 
         <div className='mb-5'>
-          <label htmlFor='owner' className=' block text-violet-600 uppercase font-bold'>
+          <label htmlFor='propietario' className=' block text-violet-600 uppercase font-bold'>
             Propietario
           </label>
-          <input id='owner' type='text' placeholder='name owner' className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md' />
+          <input
+            id='propietario'
+            type='text'
+            placeholder='Propietario'
+            className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md'
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
+          />
         </div>
 
         <div className='mb-5'>
           <label htmlFor='email' className=' block text-violet-600 uppercase font-bold'>
-            Email
+            Correo Electronico
           </label>
-          <input id='email' type='email' placeholder='email owner' className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md' />
+          <input
+            id='email'
+            type='email'
+            placeholder='email owner'
+            className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className='mb-5'>
-          <label htmlFor='alta' className=' block text-violet-600 uppercase font-bold'>
+          <label htmlFor='fecha' className=' block text-violet-600 uppercase font-bold'>
             Fecha de Ingreso
           </label>
-          <input id='alta' type='date' className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md' />
+          <input
+            id='alta'
+            type='date'
+            className=' border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md'
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
+          />
         </div>
 
         <div className='mb-5'>
           <label htmlFor='sintomas' className=' block text-violet-600 uppercase font-bold'>
             Sintomas
           </label>
-          <textarea id='sintomas' className='border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md' placeholder='Sintomas' />
+          <textarea
+            id='sintomas'
+            className='border-2 w-full p-2 mt-2 placeholder-gray-600 rounded-md'
+            placeholder='Sintomas'
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
+          />
         </div>
 
-        <input type='submit' className=' bg-violet-600 w-full p-3 text-white uppercase font-bold hover:bg-violet-800 transition-all' value='agregar' />
+        <input
+          type='submit'
+          className=' bg-violet-600 w-full p-3 text-white uppercase font-bold hover:bg-violet-800 transition-all' value='agregar'
+        />
       </form>
     </div>
   )
